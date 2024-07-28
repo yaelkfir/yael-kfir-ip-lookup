fcbee5742c025340c02a29c476cbe829a00602a2
yael kfir <yael.kfir6@gmail.com>
2024-07-28 10:11:59 +0300

    cleanup

--- src/routes/ip-lookup-route/ip-lookup-route.component.ts
+++ src/routes/ip-lookup-route/ip-lookup-route.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { IpLookupComponent } from '../../core/components/ip-lookup/ip-lookup.component';
 import { MatCardModule } from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider';
 
 @Component({
   selector: 'app-ip-lookup-route',
   standalone: true,
   templateUrl: './ip-lookup-route.component.html',
   styleUrl: './ip-lookup-route.component.scss',
-  imports: [IpLookupComponent, MatCardModule],
+  imports: [IpLookupComponent, MatCardModule, MatDividerModule],
 })
 export class IpLookupRouteComponent {}
